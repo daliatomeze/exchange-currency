6d8ab2aaa38b892423b47ff1d158f1cd0f84ed64
dalia_tomeze <181062@ppu.edu.ps>
2022-02-05 15:03:58 +0200

    add home page component

---
+++ exchange-ui/src/components/home/home.js
@@ -0,0 +1,69 @@
+import React from "react";
+import './home.css'
+// append my js code here
+// componentDidMlount () {
+    //     const script = document.createElement("script");
+    //     script.src ="./resource.js";
+    //     script.async = true;
+    //     document.body.appendChild(script);
+    // }
+
+const Home = () => {
+
+    
+
+
+  return (
+      
+    <section class="home-section">
+      <div
+        class="bgimg"
+      >
+        <div class="row center">
+          <div class="home-content">
+            <h1>The World's Trusted Currency Authority</h1>
+            <p>
+              Check exchange rates, send money internationally, and free
+              currency tools
+            </p>
+          </div>
+        </div>
+        <div class="wrapper">
+          <div class="wrapper-top center">
+            <ul>
+              <li data-li="convert">
+                <p>Convert</p>
+              </li>
+              <li data-li="send">
+                <p>Send</p>
+              </li>
+              <li data-li="charts">
+                <p>Charts</p>
+              </li>
+              <li data-li="alerts">
+                <p>Alerts</p>
+              </li>
+            </ul>
+          </div>
+          <div class="wrapper-bottom">
+            <div class="container">
+              <div class="item convert">
+                <div class="item-info">
+                  <h1>test1</h1>
+                  <p>test2</p>
+                </div>
+              </div>
+              <div class="item send">
+                <div class="item-info"></div>
+              </div>
+              <div class="item charts"></div>
+              <div class="item alerts"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
