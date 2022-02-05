1db8fa993c65e631560ce878543e9ea4332eba4e
dalia_tomeze <181062@ppu.edu.ps>
2022-02-05 14:48:45 +0200

    Home page

---
+++ exchange-ui/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header/header';
+import Home from './components/home/home';
+
+
+function App() {
+  return (
+    <div className="body">
+      <Header></Header>
+      <Home></Home>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ exchange-ui/src/components/header/header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './header.css'
+
+
+const Header = () => {
+  return (
+    <header>
+      <div class="nav center">
+        <img src={require("../../img/money-transfer-logo.png")} class="image-logo" alt="logo"/>
+        <div class="righ-side">
+          <li class="active">
+            <a href="#" class="list">
+              Converter
+            </a>
+          </li>
+          <li class="list">Send money</li>
+          <li class="list">Business & Api</li>
+          <li class="list">Tools</li>
+          <li class="list">Resources</li>
+        </div>
+      </div>
+
+      <div class="btns">
+        <li id="sign-in" class="header-btn">
+          <a href="#">Sign in</a>
+        </li>
+        <li id="get-app" class="header-btn">
+          <a href="#">Get the App</a>
+        </li>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
